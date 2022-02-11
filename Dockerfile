56474d7608f5ab5f3095e1477279319e0992425b
shodan <mega@ioexception.at>
2022-02-11 23:58:55 +0100

    flags

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM python:3
 
+ENV SECURE false
+ENV USERNAME ""
+ENV PASSWORD ""
+ENV HOST "127.0.0.1"
+ENV PORT "9091"
+ENV PATH "/transmission/"
+ENV TIMEOUT 30
+ENV DELETE false
+ENV INTERVAL 30
+ENV DEBUG false
+
 RUN mkdir /app
 
 COPY main.py /app/
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import getenv
 from time import sleep
 from transmission_rpc import Client
 from argparse import ArgumentParser, ArgumentTypeError
@@ -20,6 +21,9 @@ def main(args=None):
             print("Deleting torrents")
             client.remove_torrent([t.id for t in unregistered], delete_data=args['delete'])
 
+        if args['interval'] is None:
+            break
+
         print(f"Sleeping for {args['interval']} minutes")
         sleep(args['interval'] * 60)
 
@@ -40,23 +44,31 @@ def parse_arguments():
                                   const='https', default='http',
                                   help='Use https to connect to transmission')
     connection_group.add_argument('-u', '--username', dest='username', metavar='USER',
-                                  help='username for authentication')
+                                  help='username for authentication', default=getenv("USERNAME"))
     connection_group.add_argument('-p', '--password', dest='password', metavar='PASS',
-                                  help='password for authentication')
-    connection_group.add_argument('-H', '--host', dest='host', default="127.0.0.1",
+                                  help='password for authentication', default=getenv("PASSWORD"))
+    connection_group.add_argument('-H', '--host', dest='host', default=getenv("USERNAME", "127.0.0.1"),
                                   help='transmission rpc host')
-    connection_group.add_argument('-P', '--port', dest='port', default=9091,
+    connection_group.add_argument('-P', '--port', dest='port', default=int(getenv("PORT", 9091)),
                                   help='transmission rpc port')
-    connection_group.add_argument('--path', dest='path', default='/transmission/',
+    connection_group.add_argument('--path', dest='path', default=getenv('PATH', '/transmission/'),
                                   help='transmission rpc path')
-    connection_group.add_argument('-t', '--timeout', dest='timeout', default=30, )
+    connection_group.add_argument('-t', '--timeout', dest='timeout', default=int(getenv('timeout', 30)))
 
     parser.add_argument('--delete', help='delete removed torrent files', action='store_true')
-    parser.add_argument('-i', '--interval', type=int, default=30,
-                        help='interval between runs in minutes')
+    parser.add_argument('-i', '--interval', type=int, default=int(getenv('INTERVAL')),
+                        help='interval between runs in minutes, only runs once if not set')
     parser.add_argument('--debug', help='enable debug logging', action='store_true')
 
-    return vars(parser.parse_args())
+    args = vars(parser.parse_args())
+
+    # apply flags
+    if getenv('SECURE').lower().strip() == 'true':
+        args['protocol'] = 'https'
+    if getenv('DELETE').lower().strip() == 'true':
+        args['delete'] = True
+
+    return args
 
 
 if __name__ == '__main__':
